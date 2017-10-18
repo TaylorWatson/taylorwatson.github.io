538ee34e87282d303f478bef7e56ea448ff919b6
Taylor <taylorwatson18@icloud.com>
2017-10-18 17:40:28 -0400

    added night mode
    because ¯\_(ツ)_/¯
    
    Also cleaned some stuff up.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,12 @@ $('.changing.example .demo.segment')
 ;
 
 
+$("#night-toggle").click(function() {
+    $('body').toggleClass('night-mode');
+    return false;
+});
+
+
 
 // Image model for swift calculator portfolio segment
 $(document).ready(function() {
@@ -264,3 +270,28 @@ $(document).ready(function() {
         }
     });
 });
+
+
+$(document)
+.ready(function() {
+
+  // fix menu when passed
+  $('.masthead')
+    .visibility({
+      once: false,
+      onBottomPassed: function() {
+        $('.fixed.menu').transition('fade in');
+      },
+      onBottomPassedReverse: function() {
+        $('.fixed.menu').transition('fade out');
+      }
+    })
+  ;
+
+  // create sidebar and attach to menu open
+  $('.ui.sidebar')
+    .sidebar('attach events', '.toc.item')
+  ;
+
+})
+;
